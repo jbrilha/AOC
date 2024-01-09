3489f24d370e397ccf6f313089c896e5695a4710
jbrilha <j.brilha@campus.fct.unl.pt>
2024-01-09 11:05:50 +0000

    Corrected some things and switched to CMake

--- 2023/day1/d1p2.cpp
+++ 2023/day1/d1p2.cpp
@@ -1,8 +1,8 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
@@ -10,20 +10,20 @@ int main(int argc, char *argv[]) {
     string line;
     ifstream input(argv[1]);
 
-    map<string, char> numbers {
+    unordered_map<string, char> numbers {
         {"one", '1'},   {"two", '2'},   {"three", '3'},
         {"four", '4'},  {"five", '5'},  {"six", '6'},
         {"seven", '7'}, {"eight", '8'}, {"nine", '9'},
     };
 
     if (!input.is_open())
-        printf("FAILED\n");
+        cout << "FAILED" << endl;
 
     int calibration = 0;
     while (getline(input, line)) {
         int first_digit, last_digit;
 
-        uint32_t first_digit_pos = -1; // using unsigned so I can just compare using <
+        uintmax_t first_digit_pos = string::npos; // using unsigned so I can just compare using <
         uint32_t last_digit_pos = 0;
 
         for (const auto &number : numbers) {
@@ -32,16 +32,14 @@ int main(int argc, char *argv[]) {
             size_t found_last_number = line.rfind(number.first);
             size_t found_last_digit = line.rfind(number.second);
 
-            if (found_first_number != string::npos)
-                if (found_first_number < first_digit_pos) {
-                    first_digit_pos = found_first_number;
-                    first_digit = numbers[number.first] - 0x30;
-                }
-            if (found_first_digit != string::npos)
-                if (found_first_digit < first_digit_pos) {
-                    first_digit_pos = found_first_digit;
-                    first_digit = number.second - 0x30;
-                }
+            if (found_first_number < first_digit_pos) {
+                first_digit_pos = found_first_number;
+                first_digit = numbers[number.first] - 0x30;
+            }
+            if (found_first_digit < first_digit_pos) {
+                first_digit_pos = found_first_digit;
+                first_digit = number.second - 0x30;
+            }
             
             if (found_last_digit != string::npos) 
                 if (found_last_digit >= last_digit_pos) {
@@ -54,13 +52,9 @@ int main(int argc, char *argv[]) {
                     last_digit = numbers[number.first] - 0x30;
                 }
         }
-
-        // printf("first dig [%d] @ pos [%u]\n", first_digit, first_digit_pos);
-        // printf("last dig [%d] @ pos [%u]\n", last_digit, last_digit_pos);
-        // cout << line.c_str() << '\n' << '\n';
         calibration += first_digit * 10 + last_digit;
     }
-    printf("%d\n", calibration);
+    cout << calibration << endl;
 
     input.close();
 
